5c2d90e117512d649ab4411f7a783caf816c36f7
Arkent Nathanael <arkent.huang@gmail.com>
2023-01-06 08:41:24 +0700

    Reorganized Project Structure

--- Assets/Scripts/Game/PassCheck.cs
+++ Assets/Scripts/Game/PassCheck.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PassCheck : MonoBehaviour
 {
   private void OnTriggerEnter(Collider other) {
-    GameManager.singleton.AddScore(2);
+    GameManager.singleton.AddScore(1);
     FindObjectOfType<BallComponent>().perfectPass++;
     //Perfect Pass only read once -- PassCheck is not given in the HelixPartPrefab
     Debug.Log("Perfect Pass is increased");
